37e064d7ea422864d6191d3c9d02eff2255d64a4
Gajendra Deshpande <gcdeshpande@hotmail.com>
2024-05-30 20:39:07 +0530

    Update security.php

--- inc/security.php
+++ inc/security.php
@@ -1,1 +1,11 @@
+<?php
+function security_theme_remove_wp_version() {
+    return '';
+}
+add_filter( 'the_generator', 'security_theme_remove_wp_version' );
+
+function security_theme_disable_xmlrpc() {
+    add_filter( 'xmlrpc_enabled', '__return_false' );
+}
+add_action( 'init', 'security_theme_disable_xmlrpc' );
 
