47fcddc81ebe0c00df984b77eec85c4fecf3bf4f
Gajendra Deshpande <gcdeshpande@hotmail.com>
2024-05-30 20:32:05 +0530

    Update 404.php

--- 404.php
+++ 404.php
@@ -1,1 +1,15 @@
+<?php get_header(); ?>
+<div id="primary" class="content-area">
+    <main id="main" class="site-main">
+        <section class="error-404 not-found">
+            <header class="page-header">
+                <h1 class="page-title"><?php _e( 'Oops! That page can’t be found.', 'security-theme' ); ?></h1>
+            </header>
+            <div class="page-content">
+                <p><?php _e( 'It looks like nothing was found at this location.', 'security-theme' ); ?></p>
+            </div>
+        </section>
+    </main>
+</div>
+<?php get_footer(); ?>
 
