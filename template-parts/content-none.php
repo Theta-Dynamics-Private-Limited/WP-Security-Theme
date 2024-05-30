9d67978e545dab22c361e98bab187ecfdaa0bd19
Gajendra Deshpande <gcdeshpande@hotmail.com>
2024-05-30 20:40:09 +0530

    Update content-none.php

--- template-parts/content-none.php
+++ template-parts/content-none.php
@@ -1,1 +1,10 @@
+<section class="no-results not-found">
+    <header class="page-header">
+        <h1 class="page-title"><?php _e( 'Nothing Found', 'security-theme' ); ?></h1>
+    </header>
+    <div class="page-content">
+        <p><?php _e( 'It seems we can’t find what you’re looking for. Perhaps searching can help.', 'security-theme' ); ?></p>
+        <?php get_search_form(); ?>
+    </div>
+</section>
 
