84fe3bf7676b4b5b8b8feeab57dc07d88107c2f3
Gajendra Deshpande <gcdeshpande@hotmail.com>
2024-05-30 20:39:47 +0530

    Update enqueue.php

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -1,1 +1,7 @@
+<?php
+function security_theme_enqueue_scripts() {
+    wp_enqueue_style( 'security-theme-style', get_stylesheet_uri() );
+    wp_enqueue_script( 'security-theme-custom', get_template_directory_uri() . '/assets/js/custom.js', array(), '1.0', true );
+}
+add_action( 'wp_enqueue_scripts', 'security_theme_enqueue_scripts' );
 
