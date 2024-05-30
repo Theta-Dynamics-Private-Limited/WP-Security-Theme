e7b092544fdfb106d675ccb3bdd27434f0ebf08f
Gajendra Deshpande <gcdeshpande@hotmail.com>
2024-05-30 20:36:46 +0530

    Update searchform.php

--- searchform.php
+++ searchform.php
@@ -1,1 +1,8 @@
+<form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
+    <label>
+        <span class="screen-reader-text"><?php echo _x( 'Search for:', 'label', 'security-theme' ); ?></span>
+        <input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'security-theme' ); ?>" value="<?php echo get_search_query(); ?>" name="s">
+    </label>
+    <button type="submit" class="search-submit"><?php echo esc_attr_x( 'Search', 'submit button', 'security-theme' ); ?></button>
+</form>
 
