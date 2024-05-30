830a6e30494b3605725f68182d83773e1c6ce9b8
Gajendra Deshpande <gcdeshpande@hotmail.com>
2024-05-30 20:32:32 +0530

    Update archive.php

--- archive.php
+++ archive.php
@@ -1,1 +1,18 @@
+<?php get_header(); ?>
+<div id="primary" class="content-area">
+    <main id="main" class="site-main">
+        <?php if ( have_posts() ) : ?>
+            <header class="page-header">
+                <h1 class="page-title"><?php the_archive_title(); ?></h1>
+            </header>
+            <?php while ( have_posts() ) : the_post(); ?>
+                <?php get_template_part( 'template-parts/content', get_post_format() ); ?>
+            <?php endwhile; ?>
+            <?php the_posts_navigation(); ?>
+        <?php else : ?>
+            <?php get_template_part( 'template-parts/content', 'none' ); ?>
+        <?php endif; ?>
+    </main>
+</div>
+<?php get_footer(); ?>
 
