b214abb083bc8d45613d125e1451c745384682c5
Gajendra Deshpande <gcdeshpande@hotmail.com>
2024-05-30 20:37:04 +0530

    Update single.php

--- single.php
+++ single.php
@@ -1,1 +1,13 @@
+<?php get_header(); ?>
+<div id="primary" class="content-area">
+    <main id="main" class="site-main">
+        <?php while ( have_posts() ) : the_post(); ?>
+            <?php get_template_part( 'template-parts/content', 'single' ); ?>
+            <?php if ( comments_open() || get_comments_number() ) : ?>
+                <?php comments_template(); ?>
+            <?php endif; ?>
+        <?php endwhile; ?>
+    </main>
+</div>
+<?php get_footer(); ?>
 
