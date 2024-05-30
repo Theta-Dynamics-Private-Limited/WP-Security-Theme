c540a90604ef90d223d6de89543e71a869605b57
Gajendra Deshpande <gcdeshpande@hotmail.com>
2024-05-30 20:33:58 +0530

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,1 +1,9 @@
+<footer id="colophon" class="site-footer">
+    <div class="site-info">
+        <?php _e( 'Proudly powered by WordPress', 'security-theme' ); ?>
+    </div>
+</footer>
+<?php wp_footer(); ?>
+</body>
+</html>
 
