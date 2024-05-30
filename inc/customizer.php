3190cef54d478a2c82bdc7ec025a3cad910d13cd
Gajendra Deshpande <gcdeshpande@hotmail.com>
2024-05-30 20:38:43 +0530

    Update customizer.php

--- inc/customizer.php
+++ inc/customizer.php
@@ -1,1 +1,6 @@
+<?php
+function security_theme_customize_register( $wp_customize ) {
+    // Customizer settings and controls
+}
+add_action( 'customize_register', 'security_theme_customize_register' );
 
